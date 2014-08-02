7dfd37cfdeb49ffd8f88fd0a6f9391a2f82b2f5b
Piotr Kowalski <piecioshka@gmail.com>
2014-08-02 13:00:22 +0200

    Test file changes

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+/*global window, document */
+
 var unique = require('uniq');
+var url = require('url');
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
 console.log(unique(data));
+
+window.addEventListener('load', function () {
+    document.body.innerHTML = '<pre>' + JSON.stringify(url.parse(window.location.href));
+});
